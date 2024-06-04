79d5c654ef4079ce6f2ccb2c1c9377347110c86f
= <=>
2024-06-04 08:52:10 -0300

    Aula 4

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //console.log("Processo principal")
 const { app, BrowserWindow, nativeTheme } = require('electron')
 
+// Janela principal
 const createWindow = () => {
   //nativeTheme.themeSource = 'light'
   const win = new BrowserWindow({
@@ -15,8 +16,22 @@ const createWindow = () => {
   win.loadFile('./src/views/index.html')
 }
 
+// Janela sobre
+const aboutWindow = () => {
+  const about = new BrowserWindow({
+    width: 360,
+    height: 220,
+    icon: './src/public/img/pc.png',
+    autoHideMenuBar: true,
+    resizable: false
+  })
+
+  about.loadFile('./src/views/sobre.html')
+}
+
 app.whenReady().then(() => {
   createWindow()
+  //aboutWindow()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
